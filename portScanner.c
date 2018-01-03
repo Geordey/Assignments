beb7d00cf741f4a07c0a1fa53d9bca3b0047e370
Geordey <geordey.gatt.a100514@mcast.edu.mt>
2018-01-03 04:43:14 -0800

    Assignment

---
+++ portScanner.c
@@ -0,0 +1,158 @@
+#include <stdio.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <string.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct Ports
+{ 
+	int portNumber;
+	char service[10]; 
+	struct Ports* next;
+}port;
+
+
+/*List head */
+port *list_head = NULL;
+
+port* get_head()
+{
+	return list_head;
+}
+
+/* First Element */
+void set_head(port* new_port)
+{
+	list_head = new_port;
+}
+
+port* find_last()
+{
+	if(get_head() == NULL)
+		return NULL;
+
+	port *curr = get_head(),
+	     *last = get_head();
+
+	while(curr != NULL)
+	{	
+		if(curr->next != NULL)
+		last = curr->next;
+		curr = curr->next;
+	}
+	
+	return last;
+}
+
+void add_port(port* new_port)
+{
+	if(get_head() == NULL)
+		set_head(new_port);
+	else 
+		find_last()->next = new_port;
+}
+
+
+void generate_port(int i, char *ser)
+{
+	port *p = (port*)malloc(sizeof(port));
+	p->portNumber = i;
+	strcpy(p->service,ser);
+	p->next = NULL;
+	add_port(p);
+}
+
+int list_size()
+{
+	int total = 0;
+	port* curr = get_head();
+
+	while(curr)
+	{
+		total++;
+		curr = curr->next;
+	}
+	return total;
+}
+
+void print_list()
+{
+	port* curr = get_head();
+
+	if(curr != NULL)
+	{
+	  while(curr->next != NULL)
+		{
+		  printf("Port: %d Service %s\n",curr->portNumber,curr->service);
+	  	  curr = curr->next;  
+		}
+	}
+
+	printf("Port: %d Service %s\n",curr->portNumber,curr->service);
+}
+
+int main(int argc, char ** argv)
+{
+	int sockfd=0;
+	int min = atoi(argv[1]);
+	int max = atoi(argv[2]);
+	struct sockaddr_in serv_addr;
+	struct hostent *url;
+
+
+	for(int i = 0; i < argc; i++)
+	{
+		printf("%d, is %s\n",i,argv[i]);
+	}
+
+	sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (sockfd < 0) {
+		fprintf(stderr, "ERROR: Failed to open socket\n");
+		return 1;
+	}
+
+	url = gethostbyname(argv[3]); /* does not support IP6 */
+	if (url == NULL) {
+		fprintf(stderr, "ERROR: Host not found\n");
+		return 2;
+	}
+
+	printf("Scanning ports %d - %d\n",  min, max);
+
+	FILE * file = fopen("/home/llp2/Desktop/Assignment/ports.txt", "w");//open log file
+
+	for (unsigned short port = min; port < max; port++)//loop through and scan all ports
+	{
+			memset(&serv_addr, 0, sizeof(serv_addr));
+			serv_addr.sin_family = AF_INET;
+			memcpy(&serv_addr.sin_addr, url->h_addr, url->h_length);
+			serv_addr.sin_port = htons(port);
+	/* Connect to the server */
+		if (connect(sockfd, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) != 0) 
+		{	
+	    		//if the port is open then get information about that port and print it to the log file
+	    		char host[128];
+	    		char service[128];
+	    		getnameinfo((struct sockaddr*)&serv_addr, sizeof serv_addr, host, (sizeof host), service, sizeof service, 0);
+			// Insert in file. 
+			fprintf(file, "Port : %d, Service : %s, Open\n", port, service); 
+	
+			//print in console.
+			printf("Port : %d, Service : %s, Open\n", port, service);
+	
+			//add to linked list
+			generate_port(port,service);
+			//check that linked list is working, to be used instead of printf.
+			//print_port(p);	    		
+		}
+
+  	}
+	
+	close(sockfd); 
+    	//fclose(file);//Close the file
+
+	print_list();
+    return 0;
+}
